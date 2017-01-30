bb8e1550f2fba9c382fbea195475cfde3456c552
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-30 09:19:52 -0800

    Add "exists" test; tmpDir/tmpFile

--- files_test.go
+++ files_test.go
@@ -1,6 +1,68 @@
 package main
 
 import (
-	. "github.com/franela/goblin"
 	"testing"
+	"github.com/spf13/afero"
+	"fmt"
+	. "github.com/franela/goblin"
 )
+
+// Keeps track of files that we've used so we can clean up.
+var testRegistry []string
+
+/*
+Creates a tmp directory for us to use.
+ */
+func tmpDir() string {
+	name, err := afero.TempDir(AppFs, "", "test")
+	if err != nil {
+		panic(fmt.Sprint("unable to work with test dir", err))
+	}
+	testRegistry = append(testRegistry, name)
+
+	return name
+}
+
+/*
+Creates a tmp file for us to use when testing
+ */
+func tmpFile() afero.File {
+	file, err := afero.TempFile(AppFs, "", "test")
+	if err != nil {
+		panic(fmt.Sprint("unable to work with tmp dir", err))
+	}
+
+	testRegistry = append(testRegistry, file.Name())
+
+	return file
+}
+
+/*
+Removes all files in our test registry
+ */
+func removeAllTestFiles(t *testing.T) {
+	for _, path := range testRegistry {
+		if err := AppFs.RemoveAll(path); err != nil {
+			t.Error(AppFs.Name(), err)
+		}
+	}
+
+	testRegistry = make([]string, 0)
+}
+
+func TestExists(t *testing.T) {
+	defer removeAllTestFiles(t)
+
+	g := Goblin(t)
+	g.Describe("exists", func() {
+		g.It("should return true if the file exists", func() {
+			tmp := tmpFile()
+			fmt.Println(tmp.Name())
+			g.Assert(exists(tmp.Name())).Equal(true)
+		})
+
+		g.It("should return false if the file doesn't exist", func() {
+			g.Assert(exists("IDontExist.jpg")).Equal(false)
+		})
+	})
+}
--- thaum_files/test/{{name}}_test.go
+++ thaum_files/test/{{name}}_test.go
@@ -4,3 +4,12 @@ import (
 	. "github.com/franela/goblin"
 	"testing"
 )
+
+func Test{{name}}(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("{{name}} render()", func() {
+		g.It("Should do something", func() {
+			// Put your test here!
+		})
+	})
+}
